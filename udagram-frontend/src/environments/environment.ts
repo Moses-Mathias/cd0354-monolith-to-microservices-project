dc6da42330a9a491ed8fd7f8407df8412384ebea
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-07-17 12:15:06 +0300

    Monolith to microservices refactoring

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a039e29515c0142c8b4fdd6fb78ba1dd-1550553958.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
